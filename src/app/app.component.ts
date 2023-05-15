f8e1676b977dc953b3224efcd212bfc395016caf
villaa <villaa@epsas.com>
2023-05-15 15:26:27 -0400

    inicio de componente counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '02-bases';
+  public title:string = 'Mi primera app de Angular';
+  public counter:number = 10;
+
+  increaseBy(value:number):void{
+    this.counter +=value;
+  }
+  reset():void{
+      this.counter = 10;
+  }
 }
