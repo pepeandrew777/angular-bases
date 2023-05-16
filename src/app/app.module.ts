9b1691f7d1b4a596b57db3f1e83726bcde8a63bb
villaa <villaa@epsas.com>
2023-05-16 11:50:45 -0400

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroComponent,
-    ListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    CounterModule
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
