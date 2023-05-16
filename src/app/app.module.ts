b3ac4cf86b7ce6b4e5995ee07b48b15a091cdf0a
villaa <villaa@epsas.com>
2023-05-16 10:42:14 -0400

    inicio de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/components/counter.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
     HeroComponent,
     ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
